4e2834b23e1ca4f802c68c4b1b9cb0459e8d4746
Sejin Jeon <jinaidy93@gmail.com>
2018-11-24 23:43:03 +0900

    Add Naver search API example codes to api/index.js

--- api/index.js
+++ api/index.js
@@ -1,8 +1,27 @@
 import Router from 'koa-router';
+import axios from 'axios';
+import naverAPIInfo from '../naver-api-info';
 
 const router = new Router();
-router.get('/', async (ctx) => {
-  await ctx.render('home', 'test data');
-});
+router
+  .get('/', async (ctx) => {
+    await ctx.render('home', 'test data');
+  })
+  .get('naver-search-news', async (ctx) => {
+    const { clientID, clientSecret } = naverAPIInfo;
+
+    // const searchText = encodeURI(ctx.request.body.query);
+    const searchText = encodeURI('네이버 가고 싶다');
+    const apiURL = `https://openapi.naver.com/v1/search/news?query=${searchText}`;
+    const options = {
+      headers: {
+        'X-Naver-Client-Id': clientID,
+        'X-Naver-Client-Secret': clientSecret,
+      },
+    };
+
+    const naverSearchResult = await axios.get(apiURL, options);
+    console.log(naverSearchResult.data);
+  });
 
 export default router;
